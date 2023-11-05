0058a29172e895b1ba2c2278f08ee22e0e6c83b2
Scott Tomaszewski <scottTomaszewski@gmail.com>
2023-11-05 08:50:08 -0500

    Adds install and uninstall support

--- bashbox.bash
+++ bashbox.bash
@@ -8,14 +8,41 @@ fi
 
 BASHBOX=$0
 
-# this is a copy of bb.io.full_dir_of.  I havent sourced it yet, so I dont have it available...
-source=${BASH_SOURCE[0]}
-while [ -L "$source" ]; do # resolve $source until the file is no longer a symlink
-  src_dir=$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )
-  source=$(readlink "$source")
-  [[ $source != /* ]] && source=$src_dir/$source # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-src_dir=$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )
+if [ "$1" == "home_dir" ]; then
+	# Check BASHBOX_HOME for commands
+	if [ -n "$BASHBOX_HOME" ]; then 
+        	if [ -f "$BASHBOX_HOME/commands/preconditions.bash" ]; then
+               	src_dir="$BASHBOX_HOME"
+	        else
+        	        echo "BASHBOX_HOME has value $BASHBOX_HOME but cannot find commands."
+                	exit 1
+	        fi
+
+	# Check standard install location for commands
+	elif [ -f "/usr/local/lib/bashbox/commands/preconditions.bash" ]; then
+        	src_dir="/usr/local/lib/bashbox"
+
+	# Check relative path for commands
+	else
+        	# this is a copy of bb.io.full_dir_of.  I havent sourced it yet, so I dont have it available...
+	        source=${BASH_SOURCE[0]}
+        	while [ -L "$source" ]; do # resolve $source until the file is no longer a symlink
+                	src_dir=$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )
+	                source=$(readlink "$source")
+        	       [[ $source != /* ]] && source=$src_dir/$source # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+        	done
+	        src_dir=$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )
+
+		if [ ! -f "${src_dir}/commands/preconditions.bash" ]; then
+	                echo "Could not find bashbox commands. Exiting"
+	                exit 1
+	        fi
+	fi
+	echo "$src_dir"
+	exit 0
+fi
+
+src_dir=$($BASHBOX home_dir)
 
 # Extract the first argument as the subcommand
 subcommand="$1"
--- justfile
+++ justfile
@@ -16,4 +16,22 @@ update_readme:
 	./bashbox.bash docs.markdown_links >> README.md
 
 install_completions:
-	cp bb_completion /etc/bash_completion.d/
+	@cp bb_completion /etc/bash_completion.d/
+	@echo "Bashbox autocompletion prepared.  Please add the following to ~/.bashrc or equivalent:"
+	@echo ""
+	@echo "source /etc/bash_completion.d/bb_completion"
+
+install: uninstall && install_completions
+	#!/usr/bin/env bash
+	set -eo pipefail
+	# TODO - make this a symlink
+	cp bashbox.bash /usr/local/bin/bb
+	mkdir -p /usr/local/lib/bashbox
+	cp -R commands /usr/local/lib/bashbox/commands	
+
+uninstall:
+	#!/usr/bin/env bash
+	set -eo pipefail
+	rm -f /usr/local/bin/bb
+	rm -rf /usr/local/lib/bashbox
+	rm -rf /etc/bash_completion.d/bb_completion
